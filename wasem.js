834b275cee26e83fb976c4282fb4310caf70bbbc
Stefan Bilharz <stefan@sbsoftware.de>
2018-09-18 23:52:43 +0200

    WIP: Emscripten setjmp/longjmp handling

--- wasem.js
+++ wasem.js
@@ -11,14 +11,79 @@ export function load(path, opts) {
     __syscall4: function(nr, a1, a2, a3, a4) { return kernel.syscall.apply(kernel, arguments); },
     __syscall5: function(nr, a1, a2, a3, a4, a5) { return kernel.syscall.apply(kernel, arguments); },
     __syscall6: function(nr, a1, a2, a3, a4, a5, a6) { return kernel.syscall.apply(kernel, arguments); },
-    setjmp: function(a1) { console.debug("setjmp call"); return 0; },
-    longjmp: function(a1, a2) {
-      console.debug("longjmp call");
-      try {
-        throw new Error;
-      } catch {
-        console.debug("catched error");
+    setjmp: function(jmp_buf_ptr) {
+      console.debug("setjmp: " + jmp_buf_ptr);
+      return 0;
+    },
+    longjmp: function(jmp_buf_ptr, ret) {
+      console.debug("longjmp: " + jmp_buf_ptr + ", " + ret);
+    },
+    saveSetjmp: function(env, label, table, size) {
+      console.debug("saveSetjmp call with arguments: ");
+      console.debug(arguments);
+      let env32 = env>>2;
+      let table32 = table>>2;
+      let setjmpId = ++kernel.setjmpId;
+      console.debug("setjmpId: " + setjmpId);
+      console.debug("kernel.setjmpId: " + kernel.setjmpId);
+
+      kernel.heap32[env32] = setjmpId;
+
+      for (let i = 0; i < size; i++) {
+        if (kernel.heap32[table32 + 2*i] == 0) {
+          kernel.heap32[table32] = setjmpId;
+          kernel.heap32[table32 + 2*i + 1] = label;
+          kernel.heap32[table32 + 2*i + 2] = 0;
+        }
       }
+
+      kernel.instance.exports.setTempRet0(size);
+
+      return table;
+    },
+    emscripten_longjmp_jmpbuf: function(env, value) {
+      console.debug("emscripten_longjmp_jmpbuf call with arguments: ");
+      console.debug(arguments);
+      kernel.instance.exports.setThrew(env, value || 1);
+      throw 'longjmp';
+    },
+    "__invoke_void_%struct.__jmp_buf_tag*_i32": function(func_idx, jmp_buf_ptr, i32) {
+      console.debug("__invoke_void_%struct.__jmp_buf_tag*_i32 call with arguments: ");
+      console.debug(arguments);
+      try {
+        return kernel.indirect_function_table.get(func_idx)(jmp_buf_ptr, i32);
+      } catch(e) {
+        if (e !== 'longjmp') { throw e; }
+        kernel.instance.exports.setThrew(1, 0);
+      }
+    },
+    "__invoke_i32_i8*_...": function(func_idx, a1, a2) {
+      console.debug("__invoke_i32_i8*_... call with arguments:");
+      console.debug(arguments);
+      try {
+        return kernel.indirect_function_table.get(func_idx)(a1, a2);
+      } catch(e) {
+        if (e !== 'longjmp') throw e;
+        kernel.instance.exports.setThrew(1, 0);
+      }
+    },
+    testSetjmp: function(id, table, size) {
+      console.debug("testSetjmp call with arguments: ");
+      console.debug(arguments);
+      let table32 = table>>2;
+
+      for (let i = 0; i < size; i++) {
+        let setjmpId = kernel.heap32[table32 + 2*i];
+        if (setjmpId == 0) break;
+        if (setjmpId == id) return kernel.heap32[table32 + 2*i + 1];
+      }
+      console.debug("setjmpId lookup failed");
+      return 0;
+    },
+    emscripten_longjmp: function() {
+      console.debug("emscripten_longjmp call with arguments: ");
+      console.debug(arguments);
+      return 0;
     }
   };
 
@@ -44,6 +109,7 @@ export function load(path, opts) {
     }
     return WebAssembly.instantiate(wasm_module, {env: imports});
   }).then(function(instance) {
+    kernel.instance = instance;
     kernel.setHeapBase(instance.exports.__heap_base);
     if (typeof instance.exports.main === 'function') {
       instance.exports.main();
